318f15b99674510754b04e038ead333c9e0222b0
Hossein <rezaeig22@gmail.com>
2024-05-09 20:36:30 +0330

    using lodash for picking the right data from the request

--- routes/users.ts
+++ routes/users.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash'
 import { UserDto } from '../dtos';
 import { User, validateUser } from '../models/user';
 import express from 'express';
@@ -9,16 +10,16 @@ router.post('/', async (req, res) => {
     if (error)
         return res.status(400).send(error.details[0].message);
 
-    const { email, name, password } = req.body as UserDto
+    const { email } = req.body as UserDto
 
     let user = await User.findOne({ email })
     if (user)
         return res.status(400).send('کاربر قبلا ثبت نام کرده است')
 
-    user = new User({ name, email, password })
+    user = new User(_.pick(req.body, ['name', 'email', 'password']))
     await user.save()
 
-    return res.status(201).json(user);
+    return res.status(201).json(_.pick(user, ['_id', 'name', 'email']));
 });
 
 export default router;
