d5ed430218b735a5ecc7f347e88811fd4af32e59
Hossein <rezaeig22@gmail.com>
2024-05-12 22:00:23 +0330

    test: user.generateAuthToken

--- tests/unit/models/user.test.ts
+++ tests/unit/models/user.test.ts
@@ -1,5 +1,22 @@
-const { User } = require('../../../models/user')
+const { User } = require('../../../models/user');
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import mongoose from 'mongoose';
 
 describe('user.generateAuthToken', () => {
-    it('should return a valid JWT', () => { })
+    it('should return a valid JWT', () => {
+        // ARRANGE
+        const payload = {
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        }
+        const user = new User(payload)
+
+        // ACTION
+        const token = user.generateAuthToken()
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
+
+        // ASSERTION
+        expect(decoded).toMatchObject(payload)
+    })
 });
