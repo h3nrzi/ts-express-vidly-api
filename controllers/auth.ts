e3cc036aa641815753bebebf8ae012e3a0757f9e
Hossein <rezaeig22@gmail.com>
2024-05-15 18:06:43 +0330

    Refactor validate auth request

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -6,7 +6,7 @@ import bcrypt from 'bcrypt'
 import { AuthDto, UserDto } from '../dtos';
 import { User } from '../models/user';
 
-function validateUser(user: AuthDto) {
+export function validateUser(user: AuthDto) {
     const schema = {
         email: Joi.string().required().min(5).max(255).email(),
         password: Joi.string().required().min(5).max(1024),
@@ -16,10 +16,6 @@ function validateUser(user: AuthDto) {
 }
 
 export async function login(req: Request, res: Response) {
-    const { error } = validateUser(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message);
-
     const { email, password } = req.body as UserDto
 
     let user = await User.findOne({ email }) as any
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import * as authController from '../controllers/auth';
+import validateRequest from '../Middlewares/validateRequest';
 const router = express.Router();
 
 
-router.post('/', authController.login);
+router.post('/',
+    validateRequest(authController.validateUser),
+    authController.login
+);
 
 export default router;
